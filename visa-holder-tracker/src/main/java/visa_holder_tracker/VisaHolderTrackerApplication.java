e17a37af6c935955fce5dfd763cbbb7f851e4674
JxcobA <166840557+JxcobA@users.noreply.github.com>
2026-05-15 13:31:25 +0100

    Initial Spring Boot project

---
+++ visa-holder-tracker/src/main/java/visa_holder_tracker/VisaHolderTrackerApplication.java
@@ -0,0 +1,13 @@
+package visa_holder_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaHolderTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaHolderTrackerApplication.class, args);
+	}
+
+}
---
+++ visa-holder-tracker/src/test/java/visa_holder_tracker/VisaHolderTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package visa_holder_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaHolderTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
