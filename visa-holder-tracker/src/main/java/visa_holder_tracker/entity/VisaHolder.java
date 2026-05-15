d266f9f2ccffe75a7c7b55c3c13e81da998fa1b8
jacob adjei <jacob@adjei.co.uk>
2026-05-15 17:39:19 +0100

    Visa Holder Entity - Created VisaHolder Entity class   - Contains getters, setters, constructors and fields for the table columns - Created VisaStatus Enum class - Updated dependencies (added lombrok) - Modified application.properties with values for H2

---
+++ visa-holder-tracker/src/main/java/visa_holder_tracker/entity/VisaHolder.java
@@ -0,0 +1,48 @@
+package visa_holder_tracker.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "visa_holders")
+// The following are lombrok annotations - reduces boilerplate code
+@Getter // Generates getter at compile time
+@Setter// Generates setter at compile time
+@NoArgsConstructor// Generates no args constructor at compile time
+@AllArgsConstructor// Generates all args constructor at compile time
+@Builder // Generates a builder class for constructing objects
+public class VisaHolder {
+
+    @Id
+    private String passportNumber;
+
+    // This annotation is a bean validation - Rejects a field if it is null, an empty string or only whitespace
+    // The message parameter is returned if validation fails.
+    @NotBlank(message = "Full name is required")
+    @Column(nullable = false)
+    private String fullName;
+
+    @NotBlank(message = "Nationality is required")
+    @Column(nullable = false)
+    private String nationality;
+
+    @NotBlank(message = "Visa type is required")
+    @Column(nullable = false)
+    private String visaType;
+
+    @NotBlank(message = "Visa expiry date is required")
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    @NotBlank(message = "Entry date is required")
+    @Column(nullable = false)
+    private LocalDateTime entryDate;
+
+
+    @Enumerated(EnumType.STRING) // Determines how enum should be persisted in DB - as a String
+    @Column(nullable = false)
+    private VisaStatus status;
+}
