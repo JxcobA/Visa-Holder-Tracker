73164648c69d595f5d8462921c4c161371510eeb
newmosesx <newlifemoses4@gmail.com>
2026-05-20 15:47:15 +0100

    Authentication and Rest Controller Endpoints

--- visa-holder-tracker/src/main/java/visa_holder_tracker/VisaHolderTrackerApplication.java
+++ visa-holder-tracker/src/main/java/visa_holder_tracker/VisaHolderTrackerApplication.java
@@ -2,8 +2,9 @@ package visa_holder_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class VisaHolderTrackerApplication {
 
 	public static void main(String[] args) {
