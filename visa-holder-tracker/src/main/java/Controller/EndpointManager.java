9469b4bcb4c1230e4cd15063a54010e410ae0b7d
newmosesx <newlifemoses4@gmail.com>
2026-05-22 13:39:47 +0100

    Added JWT dependency, added jwt generation and verification. Excluded login from default login protection.

--- visa-holder-tracker/src/main/java/Controller/EndpointManager.java
+++ visa-holder-tracker/src/main/java/Controller/EndpointManager.java
@@ -1,19 +1,46 @@
 package Controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import visa_holder_tracker.GenerateJWTToken;
+
+import java.util.Map;
 
 
 @RestController
+@RequestMapping("/api/auth")
 public class EndpointManager {
 
-    @PostMapping("/api/auth/login")
-    public ResponseEntity<String> loginEndpoint(
+    private final GenerateJWTToken jwtService;
+    private final AuthenticationManager authManager;
+    public record LoginRequest(String username, String password) {}
+
+
+    public EndpointManager(GenerateJWTToken jwtService, AuthenticationManager authManager) {
+        this.jwtService = jwtService;
+        this.authManager = authManager;
+    }
+
+    @PostMapping("/test")
+    public String testEndpoint(
             @RequestBody
-            String input){
+            LoginRequest input){
 
-        System.out.println(input);
+        Authentication auth = authManager.authenticate(new UsernamePasswordAuthenticationToken(input.username(), input.password()));
+        if (auth.isAuthenticated())
+            return "Success";
 
-        return ResponseEntity.ok().body("Cool");
+        return "Failed";
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> loginEndpoint(
+            @RequestBody
+            LoginRequest input){
+        String token = jwtService.generateToken(input.username());
+        return ResponseEntity.ok(Map.of("token", token));
     }
 }
---
+++ visa-holder-tracker/src/main/java/References/file.md
@@ -0,0 +1,5 @@
+Generating JWT Token
+https://www.youtube.com/watch?v=e2xut5xpmmk
+
+Handling Json
+https://www.geeksforgeeks.org/java/working-with-json-data-in-java/
